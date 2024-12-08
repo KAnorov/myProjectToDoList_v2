3445e6048da3ec1620c4c653cb4dca3f02853801
KAnorov <kostya-anorov046@yandex.ru>
2024-12-08 19:48:21 +0300

    +1

--- pages/api/todo/[[...path]].js
+++ pages/api/todo/[[...path]].js
@@ -3,6 +3,7 @@ import { neon } from '@neondatabase/serverless';
 const sql = neon(process.env.DATABASE_URL);
 // const  rows  = await sql `SELECT * FROM todo`;
 
+
 export default async function (req, res) {
   const { path } = req.query;
   const id = path?.[0];
